50cddb15aea3046f4acecb3570d52392594da1ab
vecerniv <vecerniv@vscht.cz>
2026-08-14 10:49:34 +0200

    chore: small solver refactor

--- applications/solvers/simpleHFDIBRANSFoam/simpleHFDIBRANSFoam.C
+++ applications/solvers/simpleHFDIBRANSFoam/simpleHFDIBRANSFoam.C
@@ -31,7 +31,8 @@ Description
 
 Contributors
     Federico Municchi (2016),
-    Martin Isoz (2019-*), Martin Šourek (2019-*), Lucie Kubíčková (2021-*)
+    Martin Isoz (2019-*), Martin Šourek (2019-*), Lucie Kubíčková (2021-*),
+	Vít Večerník (2026-*)
 
 \*---------------------------------------------------------------------------*/
 
